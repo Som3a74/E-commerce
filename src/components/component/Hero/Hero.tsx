689c9f60f0a9fedf8214fff52e05c81da9247058
Som3a <som3a11202@gmail.com>
2024-10-11 03:52:16 +0300

    more fixed Hero & PopularCategories

--- src/components/component/Hero/Hero.tsx
+++ src/components/component/Hero/Hero.tsx
@@ -5,14 +5,15 @@ export default function Hero() {
     return (
         <section>
             <div className="mx-auto py-10 lg:px-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-8">
-                {HeroData.map((ele) => (
+                {HeroData.map((ele, index) => (
                     <div key={ele.title} className="group relative rounded-lg w-full h-[200px] cursor-pointer overflow-hidden">
                         <Image
                             src={ele.img}
                             alt={ele.title}
                             className="rounded-lg w-full object-cover group-hover:scale-110 transition-transform duration-300"
-                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  // Add the sizes prop
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                             fill
+                            priority={index === 0}
                         />
 
                         <div className={`absolute text-lightUi top-[10%] left-5`}>
--- src/components/component/PopularCategories.tsx
+++ src/components/component/PopularCategories.tsx
@@ -15,7 +15,7 @@ export default async function PopularCategories() {
             <SectionHadder linkText='View All Categories' text='Popular categories' link='/' show={true} />
 
             <div className='mx-auto py-10 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8'>
-                {CategoryData.data.map((ele) => (
+                {CategoryData.data.map((ele ,index) => (
                     <Link key={ele._id} href={`/product/productFilters?categoryId=${ele._id}`} className='relative h-60 w-full group overflow-hidden cursor-pointer rounded-md'>
                         <div className="relative w-full h-full">
                             <Image
@@ -24,8 +24,9 @@ export default async function PopularCategories() {
                                 fill
                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                 className="object-cover object-top rounded-md group-hover:scale-110 transition-transform duration-300"
-                                loading="lazy" 
-                                priority={false}
+                                // loading="lazy" 
+                                // priority={false}
+                                priority={index < 0 || index < 1 ? true : false} 
                             />
                         </div>
                         <span className="absolute bottom-0 pt-3 text-DarkBeLight bg-grayUI100 w-full text-center text-sm md:text-base font-bold">
